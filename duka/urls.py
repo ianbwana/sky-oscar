0e8a22168de137d569b3a9c98c589c3fce2413a8
ianbwana <ianbwana@gmail.com>
2018-12-09 01:31:01 +0300

    added url redirect for catalogue

--- duka/urls.py
+++ duka/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from oscar.app import application
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     # url(r'^i18n/', include('django.conf.urls.i18n')),
@@ -32,4 +33,6 @@ urlpatterns = [
 
     # url(r'', application.urls),
     path('', application.urls),  # > Django-2.0
+
+    path('', RedirectView.as_view(url='/catalogue/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
